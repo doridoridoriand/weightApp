a969afbcc4b596d3db84fa99c022b8fe8f236061
Dorian <i.am.eager.for.peace@gmail.com>
2014-06-20 20:00:02 +0900

    created ItemDetailFragment.java

--- ayumi/src/main/java/com/doriwo/weightappandroid/ayumi/ItemDetailFragment.java
+++ ayumi/src/main/java/com/doriwo/weightappandroid/ayumi/ItemDetailFragment.java
@@ -1,13 +1,18 @@
 package com.doriwo.weightappandroid.ayumi;
 
+import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +23,52 @@ public class ItemDetailFragment extends Fragment implements LoaderManager.Loader
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        VIew view = inflater.inflate(R.layout.)
+        VIew view = inflater.inflate(R.layout.weight_item_detail, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyVIew();
+        getLoaderManager().destroyLoader(0);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
+        Bundle b = this.getArguments();
+        Uri uri = Uri.parse(ParameterManager.Parameters.CONTENT_URI + "/" + b.getString("mass"));
+        return new CursorLoader(this.getActivity(), Uri, null, null, null, null, null,);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if (cursor.getCount() == 0) {
+            mWeights = null;
+            return;
+        }
+        mWeights = new ArrayList<Weight>();
+        if (cursor.moveToFirst()) {
+            do {
+                Weight weight = new Weight();
+                weight.set_id(cursor.getInt(0));
+                weight.set_weightmass(cursor.getString(1));
+                weight.set_lastupdate(cursor.getString(2));
+                mWeights.add(weight);
+            } while (cursor.moveToNext());
+        }
+         viewSet();
+    }
+
+    private void viewSet() {
+        Weight weight = mWeights.get(0);
+
+        TextView view = (TextView) getText().findViewById(R.id.weight_item_detail_id);
+        view.setText("Data Detail\n\nid="+ weight.get_id());
     }
 }
--- ayumi/src/main/java/com/doriwo/weightappandroid/ayumi/ParameterManager.java
+++ ayumi/src/main/java/com/doriwo/weightappandroid/ayumi/ParameterManager.java
@@ -10,7 +10,7 @@ public class ParameterManager {
     public static final String AUTHORITY = "com.doriwo.weightappandroid.ayumi.provider.weight";
 
     public static final class Parameters implements BaseColumns {
-        public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/place");
+        public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/weight");
 
         public static final String CONTENT_TYPE ="vnd.android.cursor.dir/vnd.doriwo.weightappandroid.ayumi.provider.weight";
         public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.doriwo.weightappandroid.ayumi.provider.weight";
